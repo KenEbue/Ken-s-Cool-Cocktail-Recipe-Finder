d3b20f1ea68d8058c9632c3a4ba53f58e3d5e647
Kenneth <kenneth.ebue@gmail.com>
2023-06-30 05:03:05 -0700

    wip

--- cocktail-app/src/App.js
+++ cocktail-app/src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import CocktailFinder from './components/CocktailFinder';
+import CocktailList from './components/CocktailList';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <CocktailFinder />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/cocktails">All Cocktails</Link>
+              </li>
+              {/* Add more links for other routes */}
+            </ul>
+          </nav>
+          <Switch>
+            <Route exact path="/cocktails" component={CocktailList} />
+            {/* Add more routes for other components */}
+          </Switch>
+          <CocktailFinder />
+        </header>
+      </div>
+    </Router>
   );
 }
 
